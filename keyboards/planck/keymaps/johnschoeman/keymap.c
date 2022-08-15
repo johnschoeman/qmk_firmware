6cf526d246c1c0bda4d286b3472fae78712a99df
John Schoeman <johnschoeman1617@gmail.com>
2022-08-15 09:58:42 -0400

    finish keymap

--- keyboards/planck/keymaps/johnschoeman/keymap.c
+++ keyboards/planck/keymaps/johnschoeman/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_TAB,   -----,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB, _______,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -67,11 +67,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, _______, _______, KC_MINS,  KC_EQL, _______, _______, CK_LBCR, KC_RBCR, _______, _______, KC_BSPC,
-    KC_TILD,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0, KC_BSLS,
+    _______, _______, _______, KC_MINS,  KC_EQL, _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_BSPC,
+     KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0, KC_BSLS,
     KC_LSFT, _______, _______, _______, _______, _______, _______, KC_QUOT, _______, _______, _______, KC_RSFT,
     KC_MUTE, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
+
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -85,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    _______, _______, _______, KC_MINS, KC_PLUS, _______, _______, KC_LBCR, KC_RBCR, _______, _______,  KC_DEL,
-    KC_TILD, _______, _______, KC_MINS,  KC_EQL, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_MINS, KC_PLUS, _______, _______, KC_LBRC, KC_RBRC, _______, _______,  KC_DEL,
+     KC_GRV, _______, _______, KC_MINS,  KC_EQL, _______, _______, _______, _______, _______, _______, _______,
     KC_LSFT, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_RSFT,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
@@ -122,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, QK_BOOT,   DEBUG  RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD,  KC_DEL,
+    _______, QK_BOOT,   DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD,  KC_DEL,
     _______, _______,  MU_MOD,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP,    BASE, _______,  _______,  PLOVER, _______,
     _______,  MUV_DE,  MUV_IN,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
